8a9425d37442d4998f56e1608285cabb71acef92
MRUDULA N NAIK <mrudulannaik1999@gmail.com>
2021-05-12 15:12:02 +0530

    initial commit

---
+++ SpringSecurityFacebookLoginInMemory/src/main/java/com/example/demorest/SpringSecurityFacebookLoginInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFacebookLoginInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFacebookLoginInMemoryApplication.class, args);
+	}
+
+}
